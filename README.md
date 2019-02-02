94b3cc60dbb4fc66c2cf5e1563514eff91112e12
Matthew <matthewhadwen@gmail.com>
2019-02-02 10:20:02 +0000

    Add lambda calculus description

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ The idea is to perform as much application logic in the lambda domain and stick 
 
 ## Notes
 Larger numbers entered into the Fibonacci generator still suffer from a stack size error and as such will only work up to a point. This is due to a lack of tail call optimization within JavaScript. To get around this I'll be implementing trampolining, which is a little hacky but the best solution I have at this time.
+
+
+## Lambda
+
+Lambda functions are written in the form λx.E where E represents an expression which can be one of three things:
+A variable, an application, or another expression.
+
+| Type        | Lambda | JavaScript |
+| ----------- | ------ | ---------- |
+| Variable    | x      | x          |
+| Application | fx     | f(x)       |
+| Expression  | λx.x   | x => x     |
+
+The Lambda symbol λ represents a function which has two parts, the arguments on the left, and the body on the right.
+Lambda functions only accept one argument, as such λab is shorthand for λa.λb
+Any variable in the body of the function, for example x in λx.x, that is declared in the function arguments is considered 'bound'.
+Any variable that does not, for example y in λx.xy, is considered free.
+A function that contains only 'bound' variables is called 'pure'
--- lamb.js
+++ lamb.js
@@ -1,3 +1,25 @@
+// # Lambda
+
+// ## Lambda
+// 
+// Lambda functions are written in the form λx.E where E represents an expression which can be one of three things:
+// A variable, an application, or another expression.
+// 
+// | Type        | Lambda | JavaScript |
+// | ----------- | ------ | ---------- |
+// | Variable    | x      | x          |
+// | Application | fx     | f(x)       |
+// | Expression  | λx.x   | x => x     |
+// 
+// The Lambda symbol λ represents a function which has two parts, the arguments on the left, and the body on the right.
+// Lambda functions only accept one argument, as such λab is shorthand for λa.λb
+// Any variable in the body of the function, for example x in λx.x, that is declared in the function arguments is considered 'bound'.
+// Any variable that does not, for example y in λx.xy, is considered free.
+// A function that contains only 'bound' variables is called 'pure'
+
+// ## Notes
+// Any types mentioned in the type signitures below should be considered Church encodings and not JavaScript types
+
 const { K, Id } = require('./combinators.js')
 
 // Takes no arguments and returns no value
@@ -8,44 +30,54 @@ const Nothing = () => {}
 // Boolean data types are represented as a choice
 
 // True returns first argument
+// λxy.x
 const True = x => y => x
 
 // False returns second argument
+// λxy.y
 const False = x => y => y
 
 // ## Conditionals
 
 // Receives a Lamda Boolean data type and therefore returns either the first or second argument
+// λbxy.bxy
 const If = b => x => y => b(x)(y)
 
 // ## Logic
 
 // Only returns True if both expressions are True
 // And :: Boolean -> Boolean -> Boolean
+// λxy.xyx
 const And = x => y => x(y)(x)
 
 // Returns True if either expression is True
 // Or :: Boolean -> Boolean -> Boolean
+// λxy.xxy
 const Or = x => y => x(x)(y)
 
 // Inverts the argument order
+// λbxy.byx
 const Not = b => x => y => b(y)(x)
 const Flip = Not
 
 // ## Data Types
 
-// Pair :: Any -> Any -> Boolean -> Any
+// Pair :: Any -> Any -> Boolean f -> Any
+// λabf.fab
 const Pair = a => b => f => f(a)(b)
 
 
 // Return the first argument from the pair
-// First :: Pair -> Any
+// First :: Pair <Any, Any> -> Any
+// λp.p(λxy.x)
 const First = p => p(True)
 
 // Return the second argument from the pair
 // Second :: Pair -> Any
+// λp.p(λxy.y)
 const Second = p => p(False)
 
+// λxp.Pair x (Second p)
 const ReplaceFirst = x => p => Pair(x)(Second(p))
 
 const ReplaceSecond = x => p => Pair(First(p))(x)
@@ -53,16 +85,19 @@ const ReplaceSecond = x => p => Pair(First(p))(x)
 const BiMap = f => g => p => Pair(f(First(p)))(g(Second(p)))
 
 // Phi takes a Number Pair and shifts the values left and increments the second by one
-// Pair <Number, Number> :: Pair -> Pair
+// Phi :: Pair <Number, Number> -> Pair
 const Phi = p => Pair(Second(p))(Increment(Second(p)))
 
 // Standard functional composition
+// λfgx.fgx
 const Compose = f => g => x => f(g(x))
 
 // Apply a function to a value once
+// λfx.fx
 const Once = fn => x => fn(x)
 
 // Apply a function to a value twice
+// λfx.ffx
 const Twice = fn => x => fn(fn(x))
 
 // Apply a function fn to a value n + 1 times
@@ -86,6 +121,7 @@ const Five = Increment(Four)
 // Predicates
 
 // isZero :: Number -> Boolean
+// λn.n(K False True)
 const isZero = n => n( K(False) )(True)
 
 // Arithmetic Operators
