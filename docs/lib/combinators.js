97f616f5cb644933375caa9fa8a7a292929e818e
Matthew Hadwen <matthewhadwen@gmail.com>
2026-02-01 13:19:44 +0000

    feat: add Y combinator to combinators library

--- docs/lib/combinators.js
+++ docs/lib/combinators.js
@@ -9,8 +9,15 @@ const K = x => _ => x
 // Self application or M combinator
 const M = fn => fn(fn)
 
+// Y combinator (lazy/applicative-order version for JavaScript)
+// Y = λf.(λx.f(λv.xxv))(λx.f(λv.xxv))
+// The key property: Y(f) = f(Y(f))
+// This enables recursion without named self-reference
+const Y = f => (x => f(v => x(x)(v)))(x => f(v => x(x)(v)))
+
 export {
 	Id,
 	K,
 	M,
+	Y,
 }
